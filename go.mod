9c3778f7c0f3347240f0195e3f60827f216116e3
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-06-22 17:45:14 -0300

    Implemented Consumer for Kafka; Added docker files to run Kafka locally.

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/edupooter/intensivo-golang
 go 1.18
 
 require (
-	github.com/confluentinc/confluent-kafka-go v1.8.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/confluentinc/confluent-kafka-go v1.8.2
+	github.com/google/uuid v1.3.0
 )
--- infra/repository/kafka/consumer.go
+++ infra/repository/kafka/consumer.go
@@ -7,6 +7,27 @@ type Consumer struct {
 	Topics    []string
 }
 
-func (c Consumer) Consume() {
+func newConsumer(configMap *ckafka.ConfigMap, topics []string) *Consumer {
+	return &Consumer{
+		ConfigMap: configMap,
+		Topics:    topics,
+	}
+}
 
+func (c Consumer) Consume(msgChan chan *ckafka.Message) error {
+	consumer, err := ckafka.NewConsumer(c.ConfigMap)
+	if err != nil {
+		panic(err)
+	}
+	err = consumer.SubscribeTopics(c.Topics, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		msg, err := consumer.ReadMessage(-1)
+		if err == nil {
+			msgChan <- msg
+		}
+	}
 }
